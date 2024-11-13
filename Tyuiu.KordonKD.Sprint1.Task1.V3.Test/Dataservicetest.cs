df1ca5af783b09b886928d63ee7ae910d5a06dd6
kordonksenya <cuteaudcixpie@gmail.com>
2024-11-13 18:45:19 +0500

    Task1

---
+++ Tyuiu.KordonKD.Sprint1.Task1.V3.Test/Dataservicetest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KordonKD.Sprint1.Task1.V3.Lib;
+
+namespace Tyuiu.KordonKD.Sprint1.Task1.V3.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            Dataservice ds = new Dataservice();
+            double x = 2.0;
+            double y = 5.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(2.4, res);
+        }
+    }
+}
